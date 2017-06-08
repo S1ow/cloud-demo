29e1903f774847c70cef9285e45065f0d05eda38
Skyuf <275908654@qq.com>
2017-06-08 10:03:21 +0800

    eureka简单学习

--- cloud-demo-service/src/main/java/org/cloud/demo/service/ServiceApplication.java
+++ cloud-demo-service/src/main/java/org/cloud/demo/service/ServiceApplication.java
@@ -1,9 +1,11 @@
 package org.cloud.demo.service;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableAutoConfiguration
 @SpringBootApplication
 @EnableEurekaServer
 public class ServiceApplication{
--- cloud-demo-service/src/main/java/org/cloud/demo/service/controller/UserController.java
+++ cloud-demo-service/src/main/java/org/cloud/demo/service/controller/UserController.java
@@ -1,7 +1,11 @@
 package org.cloud.demo.service.controller;
 
+import java.util.List;
+
 import org.cloud.demo.service.bo.UserService;
 import org.cloud.demo.service.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
 	@Autowired
 	private UserService userService;
@@ -26,5 +32,12 @@ public class UserController {
 	@ResponseBody
 	public User getUserById(){
 		return userService.getUserById(1);
+	}
+	
+	@RequestMapping(value="/getAllUsers",method=RequestMethod.GET)
+	@ResponseBody
+	public List<User> getAllUsers(){
+		logger.info("============访问了getAllUsers方法=================");
+		return userService.getAllUsers();
 	}
 }
