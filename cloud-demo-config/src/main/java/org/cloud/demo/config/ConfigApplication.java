0ec32c7b3b6407f49d51815ddfdbd565cd5ef8ff
Skyuf <275908654@qq.com>
2017-06-09 10:04:23 +0800

    修改配置文件为yml

--- cloud-demo-config/src/main/java/org/cloud/demo/config/ConfigApplication.java
+++ cloud-demo-config/src/main/java/org/cloud/demo/config/ConfigApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
- * Hello world!
+ * 配置服务中心
  *
  */
+
 @SpringBootApplication
+//只需要一个注解即开启了配置服务的管理
 @EnableConfigServer
 public class ConfigApplication{
 	
